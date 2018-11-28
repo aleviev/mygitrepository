215339574b73ebfc2c92ba59c4b7929645cd51f0
aleviev <arthurleviev@gmail.com>
2018-11-28 20:04:19 +0300

    added if and else

--- MySecondScript.py
+++ MySecondScript.py
@@ -1,6 +1,8 @@
 def myFunc(a):
     for i in range(1, 10):
-        print('MySecondScript')
-
+        if i == 3 or i == 5:
+            print('MySecondScript')
+        else:
+            print('Not 3 or 5')
 
 myFunc(1)
